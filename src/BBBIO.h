108b1f152cd66fe12c24481145fcd700f3fff53f
Mike Soliterman <msoliter@gmail.com>
2013-05-01 12:46:20 -0700

    adding the enum back

--- src/BBBIO.h
+++ src/BBBIO.h
@@ -34,6 +34,54 @@ Environment:
 // ----------------------------------------------------------- Type Definitions
 //
 
+typedef enum _BBBIO_INFO {
+	BBBIO_INFO_VALUE,
+	BBBIO_INFO_DIRECTION
+
+/*++
+
+Enum. Description:
+
+	This enum. defines the different fields of a pin that can be written to 
+	or read from.
+	
+Member Descriptions:
+
+	*_VALUE - the value field of a pin. This is brightness for LED pins.
+	
+	*_DIRECTION - the direction of a pin, usually "in" or "out"
+	
+--*/
+
+} BBBIO_INFO;
+
+typedef struct _BBBIO_PIN {
+	unsigned Header;
+	unsigned int Number;
+	char* Name;
+	unsigned int Index;
+
+/*++
+
+Struct Description:
+
+	This struct defines the standard form of a pin on the BeagleBone Black, for
+	use with the BBBIO library.
+  
+Member Descriptions:
+
+	Header - defines which header this pin is on: 0 - P8, 1 - P9
+  
+	Number - defines which pin number this is on that header
+  
+	Name - supplies the string name given to this pin in the documentation
+  
+	Index - supplies the number used to export/unexport this pin
+
+--*/
+
+} BBBIO_PIN;
+
 //
 // ------------------------------------------------- Public Function Prototypes
 //
